e21ec1cd65428235d19c6cb847dcf6d904e6d695
Wynand Pieters <wynandpieters@gmail.com>
2016-06-26 09:23:31 +0200

    fix starting data

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -7,9 +7,9 @@ CREATE TABLE "account" (
   "role" TEXT NOT NULL
 );
 
-INSERT INTO "account" (email, password, name, role) VALUES ('alice@example.com', 'secret', 'Alice', 'Administrator');
-INSERT INTO "account" (email, password, name, role) VALUES ('bob@example.com', 'secret', 'Bob', 'NormalUser');
-INSERT INTO "account" (email, password, name, role) VALUES ('chris@example.com', 'secret', 'Chris', 'NormalUser');
+INSERT INTO "account" (email, password, name, role) VALUES ('alice@example.com', '$2a$10$.7WiVwnfmROXvphMK64c9uz9.na1zulVszM0Z1v9O3UhuV.lj.Cs2', 'Alice', 'Administrator');
+INSERT INTO "account" (email, password, name, role) VALUES ('bob@example.com', '$2a$10$Mx5edZdtteQdosaMQ1j9MeLoDO0lRPwky3FnHrJlt.qS4yW1hD6Le', 'Bob', 'NormalUser');
+INSERT INTO "account" (email, password, name, role) VALUES ('chris@example.com', '$2a$10$6yzsMAEH2Wfr91usSwVfiOBh0URls2YyNFH1kwrrA6JuR1RQOkj9q', 'Chris', 'NormalUser');
 
 # --- !Downs
 DROP TABLE "account";
